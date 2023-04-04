5417d2ff899f7f4a3a2cd7a4ebf495a0e0ac57c1
victo <victoria.litmanovich@gmail.com>
2023-04-04 22:30:38 +0300

    remove white spaces

--- src/selenium/ProjectAsos/pages/AdidasPage.java
+++ src/selenium/ProjectAsos/pages/AdidasPage.java
@@ -7,19 +7,12 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
-
-
 public class AdidasPage {
-
     private WebDriver driver;
-
     @FindBy(className = "price_CMH3V")
     WebElement price;
-
     @FindBy (className = "productDescription_sryaw")
     WebElement product;
-
-
     public AdidasPage(WebDriver driver){
         this.driver = driver;
         PageFactory.initElements(driver,this);
@@ -27,7 +20,6 @@ public class AdidasPage {
     public void getPrice() {
         String prodPrice = price.getText();
         System.out.println("The price of the item is "+prodPrice);
-
     }
     public void getPrices (){
         List<WebElement> prices =driver.findElements(By.className("price_CMH3V"));
@@ -38,23 +30,10 @@ public class AdidasPage {
             String priceNumber = textPrice.substring(1,index);
             int priceAsInt = Integer.parseInt(priceNumber);
             System.out.println("The price of item is "+ priceAsInt);
-
         }
-
     }
-
     public void getProduct (){
         String prodName = product.getText();
         System.out.println("The name of item is "+prodName);
-
-
     }
 }
-
-
-
-
-
-
-
-
